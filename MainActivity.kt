f676a72b8de5172656071dda50d48af484d2880b
Dimadyuk <dimadyuk88@gmail.com>
2022-06-13 21:55:47 +0200

    set ClickListeners on MainActivity

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.tetris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
     var tvHighScore: TextView? = null
@@ -15,5 +18,18 @@ class MainActivity : AppCompatActivity() {
         val btnResetScore = findViewById<Button>(R.id.btn_reset_score)
         val btnExit = findViewById<Button>(R.id.btn_exit)
         tvHighScore = findViewById(R.id.tv_high_score)
+
+        btnNewGame.setOnClickListener (this::onBtnNewGameClick)
+        btnResetScore.setOnClickListener (this::onBtnResetScoreClick )
+        btnExit.setOnClickListener { exitProcess(0) }
+    }
+
+    private fun onBtnNewGameClick(view: View) {
+        val intent = Intent(this, GameActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun onBtnResetScoreClick(view: View) {
+        tvHighScore?.text = "High score: 0"
     }
 }
